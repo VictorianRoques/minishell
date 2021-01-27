6811b97c2c979ff1f014a205703cb738c8a528f5
Victorian Roques <victorianroques@Victorians-MacBook-Pro.local>
2021-01-27 08:10:57 +0100

    init 2

--- srcs/main.c
+++ srcs/main.c
@@ -1,68 +1,35 @@
 #include "../includes/minishell.h"
-// void        free_tab(char **tab)
-// {
-//     int i;
 
-//     i = 0;
-//     while (tab[i])
-//     {
-//         free(tab[i]);
-//         i++;
-//     }
-//     free(tab);
-// }
+char        *ft_getenv(char **env, char *elem)
+{
+    int i;
+    char *tmp;
 
-// static void exec_cmd(char **cmd)
-// {
-//     pid_t pid = 0;                                                                                                                                
-//     int status = 0;
-
-//     pid = fork();
-//     if (pid == -1)
-//         perror("fork");
-//     else if (pid > 0) {
-//         waitpid(pid, &status, 0);
-//         kill(pid, SIGTERM);
-//     }
-//     else {
-//         if (execve(cmd[0], cmd, NULL) == -1)
-//             perror("shell");
-//         exit(EXIT_FAILURE);
-//     }
-// }
+    i = 0;
+    while (env[i])
+    {
+        if ((tmp = ft_strnstr(env[i], elem, ft_strlen(env[i]))))
+            return (tmp + 5);
+        i++;
+    }
+    return (0);
+}
 
 
-int main(int ac, char **argv, char **envp)
+int     main(int ac, char **argv, char **env)
 {
     (void)ac;
     (void)argv;
-    (void)envp;
-//     char    buff[2048];
-//     char    **cmd;
-
-//     env->data = NULL;
-//     write(1, "$>", 3);
-//     // init_env(env, envp);
-//     while(read(0, buff, 2048) > 0)
-//     {
-//         cmd = ft_split(buff, ' ');
-//         //get_absolute_path(cmd)
-//         if (cmd[0] == NULL)
-//             printf("Command not found \n");
-//         else
-//             exec_cmd(cmd);
-//         write(1, "$>", 3);
-//         free_array(cmd);   
-//     }
-//     printf("Bye \n");
-    t_list *list = NULL;
-    t_list deux;
-    t_list trois;
-    list->content = ft_strdup("toto");
-    deux.content = "lalala";
-    trois.content = "trois";
-    list->next = &deux;
-    deux.next = NULL;
-    // ft_lstadd_back(&list, &trois);
-    printf("%s\n", list->next->next->content);
+    (void)env;
+    char buff[4096];
+    char **cmd;
+    while (42)
+    read(0, buff, 4096);
+    cmd = ft_split(buff, ' ');
+    int i = 0;
+    while(cmd[i])
+    {
+        printf("%s\n", cmd[i]);
+        i++;
+    }
 }
